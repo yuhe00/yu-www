a66a620332f72f3c9a586c96569d2c00e6366c3d
Yu He <yuhe00@gmail.com>
2018-02-11 01:25:29 +0100

    Attempt to fix WebGL on mobile

--- src/Yu/Home.elm
+++ src/Yu/Home.elm
@@ -57,44 +57,61 @@ fragmentShader =
         uniform vec2 resolution;
         varying vec3 vcolor;
 
-        float PI = 3.14;
-
-        float rand(vec2 c) {
-          return fract(sin(dot(c.xy ,vec2(12.9898,78.233))) * 43758.5453);
+        vec4 mod289(vec4 x) {
+          // return x - floor(x * (1.0 / 289.0)) * 289.0;
+          return x;
         }
 
-        float noise(vec2 p, float freq) {
-          float unit = resolution.x / freq;
-          vec2 ij = floor(p/unit);
-          vec2 xy = mod(p,unit)/unit;
-          //xy = 3.*xy*xy-2.*xy*xy*xy;
-          xy = .5*(1.-cos(PI*xy));
-          float a = rand((ij+vec2(0.,0.)));
-          float b = rand((ij+vec2(1.,0.)));
-          float c = rand((ij+vec2(0.,1.)));
-          float d = rand((ij+vec2(1.,1.)));
-          float x1 = mix(a, b, xy.x);
-          float x2 = mix(c, d, xy.x);
-          return mix(x1, x2, xy.y);
+        vec4 permute(vec4 x) {
+          return mod289(((x*34.0)+1.0)*x);
         }
 
-        float pNoise(vec2 p, int res) {
-          float persistance = .5;
-          float n = 0.;
-          float normK = 0.;
-          float f = 4.;
-          float amp = 1.;
-          int iCount = 0;
-          for (int i = 0; i<50; i++){
-            n+=amp*noise(p, f);
-            f*=2.;
-            normK+=amp;
-            amp*=persistance;
-            if (iCount == res) break;
-            iCount++;
-          }
-          float nf = n/normK;
-          return nf*nf*nf*nf;
+        vec4 taylorInvSqrt(vec4 r) {
+          return 1.79284291400159 - 0.85373472095314 * r;
+        }
+
+        vec2 fade(vec2 t) {
+          return t*t*t*(t*(t*6.0-15.0)+10.0);
+        }
+
+        // Classic Perlin noise
+        float cnoise(vec2 P)
+        {
+          vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+          vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+          Pi = mod289(Pi); // To avoid truncation effects in permutation
+          vec4 ix = Pi.xzxz;
+          vec4 iy = Pi.yyww;
+          vec4 fx = Pf.xzxz;
+          vec4 fy = Pf.yyww;
+
+          vec4 i = permute(permute(ix) + iy);
+
+          vec4 gx = fract(i * (1.0 / 41.0)) * 2.0 - 1.0;
+          vec4 gy = abs(gx) - 0.5 ;
+          vec4 tx = floor(gx + 0.5);
+          gx = gx - tx;
+
+          vec2 g00 = vec2(gx.x,gy.x);
+          vec2 g10 = vec2(gx.y,gy.y);
+          vec2 g01 = vec2(gx.z,gy.z);
+          vec2 g11 = vec2(gx.w,gy.w);
+
+          vec4 norm = taylorInvSqrt(vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11)));
+          g00 *= norm.x;
+          g01 *= norm.y;
+          g10 *= norm.z;
+          g11 *= norm.w;
+
+          float n00 = dot(g00, vec2(fx.x, fy.x));
+          float n10 = dot(g10, vec2(fx.y, fy.y));
+          float n01 = dot(g01, vec2(fx.z, fy.z));
+          float n11 = dot(g11, vec2(fx.w, fy.w));
+
+          vec2 fade_xy = fade(Pf.xy);
+          vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+          float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+          return ((2.3 * n_xy) - 0.2);
         }
 
         float getTexture(vec2 uv) {
@@ -106,15 +123,10 @@ fragmentShader =
         }
 
         void main () {
-            float time = time + 20.0;
-
             vec2 uv1 = gl_FragCoord.xy + vec2(0.0, -time * 100.0);
             vec2 uv2 = gl_FragCoord.yx + vec2(0.0, -time * 100.0);
-            float n1 = pNoise(uv1, 100);
-            float n2 = pNoise(uv2, 100);
-            // float r = pNoise(10.0 * gl_FragCoord.xy + vec2(time * 300.0, -time * 300.0 * n1 * 1.0), 1000);
-            // float g = pNoise(10.0 * gl_FragCoord.xy + vec2(time * 300.0, -time * 300.0 * n1 * 1.0), 100);
-            // float b = pNoise(10.0 * gl_FragCoord.xy + vec2(time * 300.0, -time * 300.0 * n1 * 1.0), 10);
+            float n1 = cnoise(uv1 / resolution.xy * 1.5) * 0.4;
+            float n2 = cnoise(uv2 / resolution.yx * 1.5) * 0.4;
 
             vec2 flowDirection = (gl_FragCoord.xy / resolution - 0.5) * 2.0 + vec2(n1, n2);
             float cycleTime = 6.0;
@@ -129,16 +141,15 @@ fragmentShader =
             float c2 = getTexture((gl_FragCoord.xy / resolution + flowVector2) * 64.0);
             float c = mix(c1, c2, abs(ct1 - 0.5) * 2.0);
 
-            float x = cos(n1 * gl_FragCoord.y) * 0.005 + sin(gl_FragCoord.x * sin(n2)) - 0.5;
-            // gl_FragColor = vec4(vec3(x), 1.0);
-            gl_FragColor = vec4(c - sin(n1 + n2), floor(c) + n1, floor(c) + n2, 1.0);
+            gl_FragColor = vec4(c - sin(abs(n1) + abs(n2)), floor(c) + abs(n1), floor(c) + abs(n2), 1.0);
         }
     |]
 
 
 view3d : Size -> Float -> Html msg
 view3d resolution time =
-    WebGL.toHtml
+    WebGL.toHtmlWith
+        []
         [ A.width resolution.width
         , A.height resolution.height
         , A.class "shader-canvas"
